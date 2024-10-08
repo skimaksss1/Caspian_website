49d72512b50eaddcfbb71e2eb252cc887a8142dc
skimaksss1 <skimaksss@gmail.com>
2024-10-08 20:10:24 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -277,3 +277,16 @@ document.querySelectorAll('.al_card').forEach(block => {
 });
 
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
